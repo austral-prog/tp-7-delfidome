c1da379e2d242610c25ed3f636e530bcc6be01b6
delfidome <162723242+delfidome@users.noreply.github.com>
2024-05-23 21:35:04 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,17 @@
 def enumerate_list(list):
-    return list
-
+    lista1 = []
+    for posicion,elemento in enumerate(list):
+        if elemento != "":
+            largo = len(lista1)
+            lista1.append(f"{largo}. {elemento}")
+    return lista1
+    
 
 def enumerate_backwards(list):
-    return list
+    lista1 = []
+    for posicion, elemento in enumerate(list):
+        if elemento != "":
+            largo = len(lista1)
+            lista1.append(f"{largo}. {elemento[::-1]}")
+    return lista1
+    
